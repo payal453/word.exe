f11e98f1e1d4eb0f9e0629089945ff22e5db75f0
Parth-Kedari <157357095+Parth-Kedari@users.noreply.github.com>
2024-05-25 19:36:54 +0530

    Update and rename Linear_Probing_With_Chaining_With_Replacement.c to 43_Linear_Probing_With_Chaining_With_Replacement.c

--- 43_Linear_Probing_With_Chaining_With_Replacement.c
+++ 43_Linear_Probing_With_Chaining_With_Replacement.c
@@ -1,57 +1,49 @@
 #include<stdio.h>
-void insert(int item, int arr[], int n, int prime)
-{
+int count = 0;
+void insert(int item, int arr[], int n, int prime){
     int h1=item%n,i=1;
     int key=h1;
     int h2=prime-(item%prime);
-    if(arr[h1]==-1)
-    {
+    if(arr[h1]==-1){
         arr[h1]=item;
     }
-    else
-    {
-        while(1)
-        {
+    else{
+        count++;
+        while(1){
             key=(h1+i*h2)%n;
-            if(arr[key]==-1)
-            {
+            if(arr[key]==-1){
                 arr[key]=item;
                 break;
             }
-            else
-            {
+            else{
                 i++;
             }
         }
     }
 }
-void display(int arr[], int n)
-{
+void display(int arr[], int n){
     printf("\nHash Table\n");
-    for(int i=0; i<n; i++)
-    {
+    for(int i=0; i<n; i++){
         printf("%d\t%d\n",i,arr[i]);
     }
+    printf("Total number of collisions: %d\n", count); 
 }
-void main()
-{
+
+void main(){
     int n,c,item,prime;
     printf("Enter size of hash table : ");
     scanf("%d",&n);
     printf("Enter prime no. : ");
     scanf("%d",&prime); 
     int arr[n];
-    for(int i=0; i<n; i++)
-    {
+    for(int i=0; i<n; i++){
         arr[i]=-1;
     }
     printf("1.Insert, 2.Display\n");
-    while(c!=2)
-    {
+    while(c!=2){
         printf("Enter choice : ");
         scanf("%d",&c);
-        switch(c)
-        {
+        switch(c){
         case 1:
             printf("Enter item to insert : ");
             scanf("%d",&item);
