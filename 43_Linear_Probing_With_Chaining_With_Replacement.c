d0c6a1407002beb2cd493c3e23074d8dfe464c5f
Parth-Kedari <157357095+Parth-Kedari@users.noreply.github.com>
2024-05-26 22:26:59 +0530

    Update 43_Linear_Probing_With_Chaining_With_Replacement.c

--- 43_Linear_Probing_With_Chaining_With_Replacement.c
+++ 43_Linear_Probing_With_Chaining_With_Replacement.c
@@ -1,57 +1,63 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 int count = 0;
-void insert(int item, int arr[], int n, int prime){
-    int h1=item%n,i=1;
-    int key=h1;
-    int h2=prime-(item%prime);
-    if(arr[h1]==-1){
-        arr[h1]=item;
-    }
-    else{
-        count++;
-        while(1){
-            key=(h1+i*h2)%n;
-            if(arr[key]==-1){
-                arr[key]=item;
+ void insert(int item, int arr[], int chain[], int n) {
+    int key = item % n, origKey = key;
+    if (arr[key] == -1) {
+        // If the slot is empty, directly insert the item
+        arr[key] = item;
+    } else {
+        int startKey = key; // Remember where we started to avoid infinite loops
+        while (1) {
+            if (arr[key] == -1) {
+                // Found an empty slot; insert here
+                arr[key] = item;
                 break;
+            } else if (item > arr[key]) {
+                // Replace if new item is greater
+                int oldItem = arr[key];
+                arr[key] = item; // Place the new item
+                item = oldItem; // Prepare to re-insert the old item
+                count++;
             }
-            else{
-                i++;
+            origKey = key; // Move the origin key to current
+            key = (key + 1) % n; // Move to the next slot
+            if (key == startKey) {
+                printf("Hash table is full or cannot place the item\n");
+                break; // If we come full circle, stop trying to insert
             }
         }
     }
-}
-void display(int arr[], int n){
+ }
+ void display(int arr[], int n) {
     printf("\nHash Table\n");
-    for(int i=0; i<n; i++){
-        printf("%d\t%d\n",i,arr[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d -> %d\n", i, arr[i]);
     }
     printf("Total number of collisions: %d\n", count); 
-}
-
-void main(){
-    int n,c,item,prime;
-    printf("Enter size of hash table : ");
-    scanf("%d",&n);
-    printf("Enter prime no. : ");
-    scanf("%d",&prime); 
+ }
+ int main() {
+    int n, c, item;
+    printf("Enter size of hash table: ");
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0; i<n; i++){
-        arr[i]=-1;
+    for (int i = 0; i < n; i++) {
+        arr[i] = -1;
     }
     printf("1.Insert, 2.Display\n");
-    while(c!=2){
-        printf("Enter choice : ");
-        scanf("%d",&c);
-        switch(c){
-        case 1:
-            printf("Enter item to insert : ");
-            scanf("%d",&item);
-            insert(item,arr,n,prime);
-            break;
-        case 2:
-            display(arr,n);
-            break;
+    do {
+        printf("Enter choice: ");
+        scanf("%d", &c);
+        switch (c) {
+            case 1:
+                printf("Enter item to insert: ");
+                scanf("%d", &item);
+                insert(item, arr, NULL, n); // Ignore chain
+                break;
+            case 2:
+                display(arr, n);
+                break;
         }
-    }
-}
+    } while (c != 2); // Continue until 'Display' is chosen
+    return 0;
+ }
